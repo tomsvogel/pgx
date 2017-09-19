97ecdca1e21560d3cb5a49a1006ae22a35016a18
Thomas Vogel <thomas@arkulpa.at>
2017-09-19 15:30:52 +0200

    add support for CrateDB

--- conn.go
+++ conn.go
@@ -41,11 +41,11 @@ var minimalConnInfo *pgtype.ConnInfo
 func init() {
 	minimalConnInfo = pgtype.NewConnInfo()
 	minimalConnInfo.InitializeDataTypes(map[string]pgtype.OID{
-		"int4": pgtype.Int4OID,
-		"name": pgtype.NameOID,
-		"oid":  pgtype.OIDOID,
-		"text": pgtype.TextOID,
-	})
+		"int4":    pgtype.Int4OID,
+		"name":    pgtype.NameOID,
+		"oid":     pgtype.OIDOID,
+		"text":    pgtype.TextOID,
+		"varchar": pgtype.VarcharOID})
 }
 
 // NoticeHandler is a function that can handle notices received from the
@@ -384,17 +384,20 @@ func (c *Conn) connect(config ConnConfig, network, address string, tlsConfig *tl
 func (c *Conn) initConnInfo() error {
 	nameOIDs := make(map[string]pgtype.OID, 256)
 
-	rows, err := c.Query(`select t.oid,
-	case when nsp.nspname in ('pg_catalog', 'public') then t.typname
-		else nsp.nspname||'.'||t.typname
-	end
-from pg_type t
-left join pg_type base_type on t.typelem=base_type.oid
-left join pg_namespace nsp on t.typnamespace=nsp.oid
-where (
-	  t.typtype in('b', 'p', 'r', 'e')
-	  and (base_type.oid is null or base_type.typtype in('b', 'p', 'r'))
-	)`)
+	rows, err := c.Query(`select t.oid, t.typname from pg_catalog.pg_type t`)
+
+	// rows, err := c.Query(`select t.oid,
+	// 	case when nsp.nspname in ('pg_catalog', 'public') then t.typname
+	// 		else nsp.nspname||'.'||t.typname
+	// 	end
+	// from pg_type t
+	// left join pg_type base_type on t.typelem=base_type.oid
+	// left join pg_namespace nsp on t.typnamespace=nsp.oid
+	// where (
+	// 		t.typtype in('b', 'p', 'r', 'e')
+	// 		and (base_type.oid is null or base_type.typtype in('b', 'p', 'r'))
+	// 	)`)
+
 	if err != nil {
 		return err
 	}
@@ -405,7 +408,6 @@ where (
 		if err := rows.Scan(&oid, &name); err != nil {
 			return err
 		}
-
 		nameOIDs[name.String] = oid
 	}
 
